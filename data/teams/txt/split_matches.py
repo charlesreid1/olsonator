ea5b8d91b2cfcc4d16bdcd19f824bd91e3b61d5b
Charles Reid <charlesreid1@gmail.com>
2025-01-24 07:56:06 -0800

    add updates to split matches script for creating single lists

--- data/teams/txt/split_matches.py
+++ data/teams/txt/split_matches.py
@@ -2,71 +2,60 @@ import os
 import json
 
 
+
+def dump_matches(matches_file, fwdmap_file, revmap_file):
+
+    with open(matches_file, 'r') as f:
+        lines = f.readlines()
+
+    fwd_map = {}
+    rev_map = {}
+    for line in lines:
+        tokens = [j.strip() for j in line.split("|")]
+        fwd_map[tokens[0]] = tokens[1]
+        rev_map[tokens[1]] = tokens[0]
+    
+    fpath = os.path.abspath(os.path.join('..', 'json', fwdmap_file))
+    print(f"Dumping mapping to file: {fpath}")
+    with open(fpath, 'w') as f:
+        json.dump(fwd_map, f, indent=4)
+    
+    fpath = os.path.abspath(os.path.join('..', 'json', revmap_file))
+    print(f"Dumping mapping to file: {fpath}")
+    with open(fpath, 'w') as f:
+        json.dump(rev_map, f, indent=4)
+
+def dump_list(matches_file, fwd_rev, list_file):
+    
+    with open(matches_file, 'r') as f:
+        lines = f.readlines()
+
+    final_list = []
+    for line in lines:
+        tokens = [j.strip() for j in line.split("|")]
+        if fwd_rev=='fwd':
+            final_list.append(tokens[0])
+        elif fwd_rev=='rev':
+            final_list.append(tokens[1])
+
+    fpath = os.path.abspath(os.path.join('..', 'json', list_file))
+    print(f"Dumping mapping to file: {fpath}")
+    with open(fpath, 'w') as f:
+        json.dump(final_list, f, indent=4)
+
+
 # donchess <--> sagarin
-
-with open('matched_donch_sag.txt', 'r') as f:
-    lines = f.readlines()
-
-donch2sag_map = {}
-sag2donch_map = {}
-for line in lines:
-    tokens = [j.strip() for j in line.split("|")]
-    donch2sag_map[tokens[0]] = tokens[1]
-    sag2donch_map[tokens[1]] = tokens[0]
-
-fpath = os.path.abspath(os.path.join('..', 'json', 'donch2sag.json'))
-print(f"Dumping donch2sag mapping to file: {fpath}")
-with open(fpath, 'w') as f:
-    json.dump(donch2sag_map, f, indent=4)
-
-fpath = os.path.abspath(os.path.join('..', 'json', 'sag2donch.json'))
-print(f"Dumping sag2donch mapping to file: {fpath}")
-with open(fpath, 'w') as f:
-    json.dump(sag2donch_map, f, indent=4)
-
+dump_matches('matched_donch_sag.txt', 'donch2sag.json', 'sag2donch.json')
 
 # donchess <--> kenpom
-
-with open('matched_donch_kenpom.txt', 'r') as f:
-    lines = f.readlines()
-
-donch2kenpom_map = {}
-kenpom2donch_map = {}
-for line in lines:
-    tokens = [j.strip() for j in line.split("|")]
-    donch2kenpom_map[tokens[0]] = tokens[1]
-    kenpom2donch_map[tokens[1]] = tokens[0]
-
-fpath = os.path.abspath(os.path.join('..', 'json', 'donch2kenpom.json'))
-print(f"Dumping donch2kenpom mapping to file: {fpath}")
-with open(fpath, 'w') as f:
-    json.dump(donch2kenpom_map, f, indent=4)
-
-fpath = os.path.abspath(os.path.join('..', 'json', 'kenpom2donch.json'))
-print(f"Dumping kenpom2donch mapping to file: {fpath}")
-with open(fpath, 'w') as f:
-    json.dump(kenpom2donch_map, f, indent=4)
-
+dump_matches('matched_donch_kenpom.txt', 'donch2kenpom.json', 'kenpom2donch.json')
 
 # donchess <--> teamrankings
+dump_matches('matched_donch_teamrankings.txt', 'donch2teamrankings.json', 'teamrankings2donch.json')
 
-with open('matched_donch_teamrankings.txt', 'r') as f:
-    lines = f.readlines()
-
-donch2teamrankings_map = {}
-teamrankings2donch_map = {}
-for line in lines:
-    tokens = [j.strip() for j in line.split("|")]
-    donch2teamrankings_map[tokens[0]] = tokens[1]
-    teamrankings2donch_map[tokens[1]] = tokens[0]
-
-fpath = os.path.abspath(os.path.join('..', 'json', 'donch2teamrankings.json'))
-print(f"Dumping donch2teamrankings mapping to file: {fpath}")
-with open(fpath, 'w') as f:
-    json.dump(donch2teamrankings_map, f, indent=4)
-
-fpath = os.path.abspath(os.path.join('..', 'json', 'teamrankings2donch.json'))
-print(f"Dumping teamrankings2donch mapping to file: {fpath}")
-with open(fpath, 'w') as f:
-    json.dump(teamrankings2donch_map, f, indent=4)
+# non-map lists
+dump_list('matched_donch_sag.txt',          'rev', 'sag.json')
+dump_list('matched_donch_kenpom.txt',       'rev', 'kenpom.json')
+dump_list('matched_donch_teamrankings.txt', 'rev', 'teamrankings.json')
+dump_list('matched_donch_kenpom.txt',       'rev', 'donch.json')
 
