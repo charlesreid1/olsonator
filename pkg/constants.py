0bc55254e37df1d27dca64ab6be11fd5db7a7e0a
Charles Reid <charlesreid1@gmail.com>
2025-01-26 04:16:36 -0800

    pre-load team names to reduce i/o

--- pkg/constants.py
+++ pkg/constants.py
@@ -1,2 +1,29 @@
+import os
+from .utils import load_json
+
+
+PKG_ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+TEAM_DIR = os.path.join(PKG_ROOT, 'data', 'teams', 'json')
+
+
+##########################
+# Name-related constants
+
+DONCH_TEAMS  = load_json(TEAM_DIR, 'donch.json')
+KENPOM_TEAMS = load_json(TEAM_DIR, 'kenpom.json')
+TR_TEAMS     = load_json(TEAM_DIR, 'teamrankings.json')
+
+# Mappings from one to the other
+DONCH2KENPOM_MAP = load_json(TEAM_DIR, 'donch2kenpom.json')
+KENPOM2DONCH_MAP = load_json(TEAM_DIR, 'kenpom2donch.json')
+
+DONCH2TR_MAP = load_json(TEAM_DIR, 'donch2teamrankings.json')
+TR2DONCH_MAP = load_json(TEAM_DIR, 'teamrankings2donch.json')
+
+
+##########################
+# Sport-related constants
+
 # How many points does home court advantage afford
 HOME_ADVANTAGE = 3.09
+
--- pkg/utils.py
+++ pkg/utils.py
@@ -1,8 +1,23 @@
+import json
+import os
+
+
 def repl(text):
     chars = "'-.& "
     for c in chars:
         text = text.replace(c, "_")
     return text
+
+
+def load_json(path_to_file, fname):
+    """
+    Load a list of team names used by a particular site
+    """
+    fpath = os.path.join(path_to_file, fname)
+    with open(fpath, 'r') as f:
+        d = json.load(f)
+    return d
+
 
 def assert_required_keys_present(d, keys):
     """
