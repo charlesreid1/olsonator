2350ab0ecb0464c7b08b315ec16c6f0e61d915e5
Charles Reid <charlesreid1@gmail.com>
2025-01-31 14:02:53 -0800

    fixup of harness

--- pkg/harness.py
+++ pkg/harness.py
@@ -7,30 +7,18 @@ class ModelDataHarness(object):
     def __init__(self, model_parameters: dict):
         self.model_parameters = model_parameters
 
-    def prepare(self, game: dict):
+    def prepare(self, game_parameters: dict):
         """
-        Prepare (download and scrape) all data that models
-        require to make a game prediction.
+        Prepare (download and scrape) data needed
+        to make a game prediction for a single game.
         """
         # Procedure:
-        # - check game date requested
-        # - open selenium/requests
-        # - ask for teamrankings pages:
-        #    - Tempo -> go to correct date
-        #    - Offensive efficiency -> go to correct date
-        #    - Defensive efficiency -> go to correct date
-        # - (use a TeamRankings wrapper class, dump results)
-        # - (we don't want to handle any results here - just download)
+        # - create a data scraper, pass it model parameters
+        # - ask it to fetch the date in game_parameters
 
         # Date, with dashes
-        game_date = game['game_date']
+        game_date = game_parameters['game_date']
 
-        ### # TODO: Ask for kenpom ratings if game was within 90 days
-        ### game_dt = datetime.strptime(game_date, "%Y-%m-%d")
-        ### diff = game_dt - datetime.today()
-        ### within_90_days = diff < timedelta(days=90)
-
-        # Ask for teamrankings.com pages
+        # Scraper for teamrankings.com data pages
         tr = TeamRankingsDataScraper(self.model_parameters)
         tr.fetch_all(game_date)
-
