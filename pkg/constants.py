96329f3880b1243c5d97b5eee8d5b4c5fafbd4c4
Charles Reid <charlesreid1@gmail.com>
2025-02-02 13:55:04 -0800

    relative level of confidence in picks from various divisions (temp)

--- pkg/constants.py
+++ pkg/constants.py
@@ -38,3 +38,42 @@ GEO_LATLONG = load_json(TEAM_DIR, 'geo_latlong.json')
 # How many points does home court advantage afford
 HOME_ADVANTAGE = 3.09
 
+
+#######################################
+# Relative confidence levels 
+# for predictions of this conference
+
+CONFIDENCES = dict(
+    A10=0,
+    ACC=-10,
+    AE=-4,
+    ASun=-13,
+    Amer=7,
+    B10=-9,
+    B12=-7,
+    BE=0,
+    BSky=7,
+    BSth=-6,
+    BW=-14,
+    CAA=-7,
+    CUSA=15,
+    Horz=-3,
+    Ivy=4,
+    MAAC=-1,
+    MAC=-12,
+    MEAC=-1,
+    MVC=-2,
+    MWC=-3,
+    NEC=-10,
+    OVC=-7,
+    PL=2,
+    SB=0,
+    SC=-17,
+    SEC=6,
+    SWAC=-9,
+    Slnd=11,
+    Sum=-14,
+    WAC=2,
+    WCC=3,
+)
+
