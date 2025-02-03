8ccee1a3d275023fc72d230940b3ff1801a743f7
Charles Reid <charlesreid1@gmail.com>
2025-02-03 11:10:11 -0800

    update drivers

--- drivers/kenpom_conference_backtest.py
+++ drivers/kenpom_conference_backtest.py
@@ -38,9 +38,9 @@ def backtest():
 def do_backtests(confs, model):
     for c in confs:
         conf_teams = [k for k,v in CONFERENCES.items() if v == c]
-        backtester = KenpomBacktester(model, start_date="2024-11-04", end_date="2025-01-31", teams=conf_teams)
+        backtester = KenpomBacktester(model, start_date="2024-11-04", end_date="2025-02-01", teams=conf_teams)
         backtester.prepare()
-        backtester.backtest(test_name=f"backtest_{c}")
+        backtester.backtest(test_name=f"backtest_kenpom_{c}")
 
 
 def print_teams(confs):
